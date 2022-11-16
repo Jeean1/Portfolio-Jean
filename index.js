6afc6bb18f638b4157a22da90c18840f1d494dc7
Jean <giraldojeanpool@hotmail.com>
2022-11-16 13:18:43 -0500

    added scroll style

--- index.js
+++
@@ -1,4 +0,0 @@
-console.log("hola");
-
-// Sé qué lograré mis sueños. Debo tener confianza en mi mismo y no desfallecer. Cada día qué me esfuerze es un día más
-// de mi meta. No dudar nunca. Lo lograré. Tengo fé 
